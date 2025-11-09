63d76abad0c1decbb8d03286d5f19907c49e282b
Vladislav Chelakov <chelakov@gmail.com>
2025-11-09 23:14:39 +0100

    feat: Initialize TurtleRocket Time Twister React project

---
+++ turtlerocket-time-twister/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App" data-testid="app-container">
+      <header className="App-header">
+        <h1>TurtleRocket Time Twister</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
